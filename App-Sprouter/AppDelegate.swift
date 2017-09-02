ce90dc0f9aca12d4d1a48aee2f557272fc465bf8
Humza Shahid <humza.shahid@ilsainteractive.com>
2017-09-02 17:41:58 +0400

    commit

--- App-Sprouter/AppDelegate.swift
+++ App-Sprouter/AppDelegate.swift
@@ -19,9 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         // Sample Code for main screen 
         // Other code is here 
+        // add item for pull request 1. 
+        // my task 1 branch test .
         return true
     }
-
+    
+    func somefunction(){
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
